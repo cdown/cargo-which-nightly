426a1987b5128d86dba1e32f7350d17b9040446b
Chris Down <chris@chrisdown.name>
2023-05-04 14:13:09 +0100

    Support finding latest common nightly

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use chrono::NaiveDate;
 use serde_json::Value;
 use std::collections::HashMap;
 
-async fn feature_dates(feature: &str, host: &str) -> Result<Vec<NaiveDate>> {
+async fn feature_dates(feature: String, host: String) -> Result<Vec<NaiveDate>> {
     let url =
         format!("https://rust-lang.github.io/rustup-components-history/{host}/{feature}.json");
     let response = reqwest::get(&url).await?;
@@ -16,11 +16,41 @@ async fn feature_dates(feature: &str, host: &str) -> Result<Vec<NaiveDate>> {
     Ok(avail_dates)
 }
 
+async fn latest_common_nightly(features: Vec<String>, host: String) -> Result<NaiveDate> {
+    let mut handles = Vec::with_capacity(features.len());
+
+    if features.is_empty() {
+        bail!("No features provided");
+    }
+
+    for feature in &features {
+        handles.push(tokio::spawn(feature_dates(feature.clone(), host.clone())));
+    }
+
+    let mut feat_dates = Vec::with_capacity(features.len());
+    for handle in handles {
+        feat_dates.push(handle.await??);
+    }
+    feat_dates[0].sort_by(|a, b| b.cmp(a)); // Latest first
+
+    for date in &feat_dates[0] {
+        if feat_dates[1..].iter().all(|dates| dates.contains(date)) {
+            return Ok(*date);
+        }
+    }
+
+    bail!("No common dates found")
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     println!(
-        "{:?}",
-        feature_dates("miri", "x86_64-unknown-linux-gnu").await
+        "{}",
+        latest_common_nightly(
+            vec!["miri".to_owned(), "clippy".to_owned(), "rls".to_owned()],
+            "x86_64-unknown-linux-gnu".to_owned()
+        )
+        .await?
     );
     Ok(())
 }
